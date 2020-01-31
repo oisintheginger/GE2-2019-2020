78d68bc9ba88fac649fe86ba6e9ff558f9072f69
skooter500 <skooter500@gmail.com>
2020-01-31 11:01:23 +0000

    added seek code & integration function

---
+++ GE2 Examples 2020/Assets/BigBoid.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BigBoid : MonoBehaviour
+{
+    public Vector3 velocity = Vector3.zero;
+    public Vector3 acceleration = Vector3.zero;
+    public Vector3 force = Vector3.zero;
+
+    public float mass = 1.0f;
+
+    public float maxSpeed = 5;
+    public float maxForce = 10;
+
+    public float speed = 0;
+
+    public bool seekEnabled = false;
+    public Vector3 target;
+    public Transform targetTransform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawSphere(target, 0.1f);
+
+        Gizmos.color = Color.blue;
+        Gizmos.DrawLine(transform.position, transform.position + force);
+
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawLine(transform.position, transform.position + velocity);
+    }
+
+    Vector3 Seek(Vector3 target)
+    {
+        Vector3 toTarget = target - transform.position;
+        Vector3 desired = toTarget.normalized * maxSpeed;
+
+        return desired - velocity;
+    }
+
+    public Vector3 CalculateForce()
+    {
+        Vector3 force = Vector3.zero;
+        if (seekEnabled)
+        {
+            force += Seek(target);
+        }
+        return force;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (targetTransform != null)
+        {
+            target = targetTransform.position;
+        }
+        force = CalculateForce();
+        acceleration = force / mass;
+        velocity += acceleration * Time.deltaTime;
+
+        transform.position += velocity * Time.deltaTime;
+        speed = velocity.magnitude;
+        if (speed > 0)
+        {
+            transform.forward = velocity;
+        }
+    }
+}
